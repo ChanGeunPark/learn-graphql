750fc8ddb6461689bc9e532d39fee00ebd0176b8
박찬근 <design795@naver.com>
2022-08-21 08:33:02 +0900

    study graphQL

--- server.js
+++ server.js
@@ -1,30 +1,148 @@
 import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
+
+let tweets = [
+  {
+    id: "1",
+    text: "first one!",
+    userId: "1",
+  },
+  {
+    id: "2",
+    text: "second one",
+    userId: "2",
+  },
+];
+
+let users = [
+  {
+    id: "1",
+    firstName: "Alvin",
+    lastName: "Park",
+  },
+  {
+    id: "2",
+    firstName: "Elon",
+    lastName: "Mask",
+  },
+];
 
 const typeDefs = gql`
+  """
+  user data
+  """
   type User {
     id: ID!
-    username: String!
+    firstName: String!
+    lastName: String!
+    """
+    Is the sum of firstName + lastName as a string
+    """
+    fullName: String!
   }
+
+  """
+  Tweet object represents a resource form a tweet
+  """
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
+
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String!
+    yt_trailer_code: String!
+    language: String!
+    mpa_rating: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
+  }
+
   type Query {
+    allUsers: [User!]!
     allTweets: [Tweet!]!
-    tweet(id: ID): Tweet
+    tweet(id: ID!): Tweet
+    ping: String!
+    allMovies: [Movie!]!
+    movie(id: String!): Movie
   }
   type Mutation {
-    postTweet(text: String, userId: ID!): Tweet
+    postTweet(text: String!, userId: ID!): Tweet!
     deleteTweet(id: ID!): Boolean!
   }
-`; //gql은 스키마를 설정해줘야한다.(type)
-//서버에서 쿼리를 불러오면 값이 null이라고 뜨는데 API가 생긴 모양을 graphql에 설명해줘야 한다.
-// [] 와 User는 관계에 따라 결정된다. User는 Tweet이 하나의 auther를 갖는다는 말이고 []는 여러개의 Tweet을 준다는 말이다.
+`;
 
-// post를 하려면 Mutation type을 사용해야 한다. user가 tweet을 post하려 하려면, tweet의 내용을 줘야한다.
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      console.log(id);
+      return tweets.find((tweet) => tweet.id === id);
+    },
+    allUsers() {
+      return users;
+    },
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((response) => response.json())
+        .then((json) => json.data.movies);
+    },
+    movie(_, { id }) {
+      return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        .then((response) => response.json())
+        .then((json) => json.data.movie);
+    },
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const findUser = users.find((user) => user.id === userId);
+      if (!findUser) return false;
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      return true;
+    },
+  },
+  User: {
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
+    },
+  },
+  Tweet: {
+    author({ userId }) {
+      return users.find((user) => user.id === userId);
+    },
+  },
+};
 
-const server = new ApolloServer({ typeDefs }); //Query type없이는 서버가 시작하지 않는다.
+const server = new ApolloServer({ typeDefs, resolvers }); //Query type없이는 서버가 시작하지 않는다.
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
